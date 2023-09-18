506bbf2fddfc45eda8a7de5ab5a6366e2a4cf741
1_dal <faberliccca@gmail.com>
2023-09-18 21:59:02 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+
+using namespace std;
+int main(){
+	setlocale(LC_ALL, "rus");
+	int a, b, c;
+	cout << "введите двухзначное число:";
+	cin >> a;
+	b = a % 10;
+	c = a / 10;
+	cout <<"сумма цифр:"<< b + c << endl << "произведение цифр:" << b * c ;
+	
+}
